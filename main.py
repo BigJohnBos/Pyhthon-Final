01b5757688844bffb382ccd7e687f0c10108e28f
BigJohnBos <boston.ballash@ucas-edu.net>
2025-09-25 14:41:37 -0600

    Co-authored-by: jezux7 <jezux7@users.noreply.github.com> Co-authored-by: boomboya <boomboya@users.noreply.github.com> Co-authored-by: archie-buss <archie-buss@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,100 @@
+# JD, AK, BB, JL 7th 
+
+
+
+# Adventure Game Final
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# archie
+
+def Right():
+
+    
+    print("you went right!")
+    print(f"{name}, you fell into a portal and teleport to the midieval times!")
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Jesus
+
+print("When you go out of school you decied to forward to wallmart ")
+print("")
+print("left = you help her")
+print("right = you push the cow over")
+print("middle = ")
+desition = input("What you gonna do? go fro the cheif or nothing?")
+
+if desition == "go for the cheif":
+    print("You run out and follow the thief for a while and even though you get tired and think about giving up, a strange adrenaline invades you and you decide to keep running to catch him without realizing that you have reached a forest.")
+
+
+
+
+
+
+
+
+
+
+#jovie lineback
+
+print("You get out of school and turn left to go go your friends house!")
+print("As you're walking to your friends house when you accidently trip and everything goes black")
+print("you wake up and someone kidnapped you and is asking for $100000000, do you have enough?")#.capitalizelize().strip()#
+#you leave her alone
+
+x = 1
+
+if x == 1:
+        print("left = you help her")>= 
+    
+    
+    
+    
+    # Boston
+
+
+
+print("The crazy adventures of Ms. LaRosa")
+name = input("What is your name")
+print(f"Welcome {name} to The crazy adventures of Ms. LaRosa!")
+print("you come to a crossroad and you can go Forward, Left, or Right")
+direction_1 = input("Forward, Left, or Right").capitalizelize().strip()
+if direction_1 == "Right":
+    Right()
+elif direction_1 == "Forward":
+     print("hi")
